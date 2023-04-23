13558a61e37eeadc9179933fa86166a3cc17cd8c
Harsha Vardhan <charagon@usc.edu>
2023-04-23 11:10:30 -0700

    changed DB name

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -6,7 +6,7 @@ export default {
     AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
     AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
     AWS_REGION: process.env.AWS_REGION,
-    DATABASE_NAME: process.env.DATABASE_NAME,
+    CITY_WATCH_DATABASE_NAME: process.env.CITY_WATCH_DATABASE_NAME,
     EMAIL_INDEX: process.env.EMAIL_INDEX,
     PRIMARY_CITY_INDEX: process.env.PRIMARY_CITY_INDEX,
     AUTH_KEY: process.env.AUTH_KEY
